102fe0f10fc875dfc61f825226795cd19094bf80
백종열 <geeksbaek@gmail.com>
2016-01-12 01:25:01 +0900

    테스트 커밋

---
+++ app/server.js
@@ -0,0 +1,35 @@
+/* global __dirname */
+var express = require('express');
+var app = express();
+
+var basedir = __dirname + '/public';
+
+app.set('views', './views')
+app.set('view engine', 'jade')
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html', {'root': basedir});
+});
+
+var server = app.listen(80, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Example app listening at http://%s:%s', host, port);
+});
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+	
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+app.use(express.static('public'));
